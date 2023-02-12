806a76559246c0051a9b680676cd1cf596f3092d
agalkartik <93568183+agalkartik@users.noreply.github.com>
2023-02-12 11:40:32 +0530

    Create Expert setter.cpp

---
+++ Expert setter.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int z=y*100/x;
+	    if(z>=50){
+	    cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
